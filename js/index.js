d156d8b68551903e53a7aaf9960c7120160d94af
Evandro Zanatta <hemanstifler@gmail.com>
2014-02-12 13:29:58 -0200

    Atualização

--- js/index.js
+++ js/index.js
@@ -1,32 +1,27 @@
 $(document).ready(function(){
-    
-    function getUrlVars(){
-
-        var vars = [], hash;
-        var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
-        for(var i = 0; i < hashes.length; i++)
-        {
-            hash = hashes[i].split('=');
-            vars.push(hash[0]);
-            vars[hash[0]] = hash[1];
-        }
-        return vars;
-    }
+       
+    var first = getUrlVars()["token"];
+    alert(first);
 
     $("#spl_entrar").click(function() {
                 
-        url = "https://api.instagram.com/oauth/authorize/?client_id=290eaa1efc0e4d658f71a35389e8f6b1&redirect_uri=http://" + window.location.host + window.location.pathname + "/&response_type=code";
+        url = "https://api.instagram.com/oauth/authorize/?client_id=290eaa1efc0e4d658f71a35389e8f6b1&redirect_uri=http://" + window.location.host + window.location.pathname + "&response_type=code";
         
-        $("body").data("token", 52);
+        //$("body").data("token", 52);
         
+        //Pega o valor do token
         token = $("body").data("token");
       
-        if(token == null){
+        //Verifica se o usuario está logado ou nao
+        //Se estiver logado, então vai pra segunda pagina
+        if(token != null){
           alert("oi");
             $.mobile.navigate( "#page", { transition : "slide"} );
+        
+        //Se não estiver logado redireciona 
+        //para a API do instagram
         }else{
-            window.open(url, '_blank');
-           
+            $(window.document.location).attr('href', URL);
         }
 
     });
@@ -35,6 +30,6 @@ $(document).ready(function(){
        alert('mudou');
     });*/
     
-    //var first = getUrlVars()["me"];
+    
    
 });
